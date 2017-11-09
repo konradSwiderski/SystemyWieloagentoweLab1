31b196c35ef2f12bf1a32561f1900e6058a19121
konradSwiderski <konrad.swiderski94@gmail.com>
2017-11-10 00:09:50 +0100

    Handle message from server

--- src/ClientAgent.java
+++ src/ClientAgent.java
@@ -1,12 +1,15 @@
 import jade.core.AID;
 import jade.core.Agent;
+import jade.core.behaviours.Behaviour;
+import jade.core.behaviours.TickerBehaviour;
 import jade.domain.DFService;
 import jade.domain.FIPAAgentManagement.DFAgentDescription;
 import jade.domain.FIPAAgentManagement.ServiceDescription;
 import jade.domain.FIPAException;
-
-
+import jade.lang.acl.ACLMessage;
 import java.util.Vector;
+import java.util.concurrent.ThreadLocalRandom;
+
 public class ClientAgent extends Agent{
 
     private Vector<AID> vectorOfServers = new Vector<>();
@@ -17,6 +20,42 @@ public class ClientAgent extends Agent{
     protected void setup()
     {
         super.setup();
+        Behaviour requestingBehaviour = new TickerBehaviour(this,ThreadLocalRandom.current().nextInt(200, 2000 + 1) )
+        {
+            protected void onTick()
+            {
+                vectorOfServers.clear();
+                searchServers();
+                ACLMessage msg = myAgent.receive();
+                if(msg != null)
+                {
+                    if (msg.getPerformative() == ACLMessage.AGREE)//Token
+                    {
+
+                    }
+                    else if (msg.getPerformative() == ACLMessage.INFORM)//Not yet
+                    {
+
+                    }
+                    else if (msg.getPerformative() == ACLMessage.FAILURE)//The end
+                    {
+
+                        doDelete();
+                    }
+                }
+                else//Send request
+                {
+                    if(vectorOfServers.isEmpty() != true)
+                    {
+                            ACLMessage reply = new ACLMessage(ACLMessage.REQUEST);
+                            reply.addReceiver(vectorOfServers.firstElement());
+                            myAgent.send(reply);
+                    }
+                        block();
+                }
+            }
+        };
+        addBehaviour(requestingBehaviour);
     }
 
     /////////////////////////////////////////////////////////////////////////////////////////////////////////////
